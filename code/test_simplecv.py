a92479a2e3f1191f5187e9b7616ef7d5958367ba
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2014-06-14 14:43:57 -0300

    Code clean-up. Created API class.

--- code/test_simplecv.py
+++ code/test_simplecv.py
@@ -1,72 +1,74 @@
-# Import the parts we need from SimpleCV
-from SimpleCV import Camera, VideoStream, Display
-
-# This import is just to create the video (if you want to do something meanwhile)
-from multiprocessing import Process
-
-# To give the correct name to the output file
+import getopt
+import sys
 import time
 
-# Imports to treat command line arguments
-import sys
-import getopt
+from collections import deque
+from multiprocessing import Process
+from SimpleCV import Camera, VideoStream, Display, cv2
 
-def main(cameraNumber, camWidth, camHeight, outputFile):
-    BUFFER_NAME = 'buffer.avi'
 
-    # create the video stream for saving the video file
-    vs = VideoStream(fps=24, filename=BUFFER_NAME, framefill=True)
+# Configuration
+HELP_MSG = """record.py [options]
 
-    # create a display with size (width, height)
-    disp = Display((camWidth, camHeight))
+    -c <cam NR>   If you know which cam you want to use: set it here, else the first camera available is selected
 
-    # Initialize Camera
-    cam = Camera(cameraNumber, prop_set={width: camWidth, height: camHeight})
+    -x <cam Width>    The width of camera capture. Default is 640 pixels
+    --width
 
-    # while the user does not press 'esc'
-    while disp.isNotDone():
-        # KISS: just get the image... don't get fancy
-        img = cam.getImage()
+    -y <cam Height>   The height of camera capture. Default is 480 pixels
+    --height
 
-        # write the frame to videostream
-        vs.writeFrame(img)
+    -o <output>       The name of the output file. Default is the timestmp
+    --output
 
-        # show the image on the display
-        img.save(disp)
+    -h <help>     Show this message
 
-    # Finished the acquisition of images now Transform into a film
-    self.makefilmProcess = Process(target=self.saveFilmToDisk, args=(BUFFER_NAME, outputFile))
-    self.makefilmProcess.start()
+"""
+BUFFER_NAME = 'buffer.avi'
+FRAME_WIDTH = 800
+FRAME_HEIGHT = 600
 
-    def saveFilmToDisk(self, bufferName, outname):
-        # construct the encoding arguments
-        params = " -i {0} -c:v mpeg4 -b:v 700k -r 24 {1}".format(bufferName, outname)
+class Watcher(object):
 
-        # run avconv to compress the video since ffmpeg is deprecated (going to be).
-        call('avconv'+params, shell=True)
+    def __init__(self,
+                 camera_id,
+                 output_fname,
+                 show_display=False,
+                 width=FRAME_WIDTH,
+                 height=FRAME_HEIGHT,
+                 fps=24,
+                 video_buffer_fname=BUFFER_NAME):
+        self.video_streamer = VideoStream(fps=fps, filename=BUFFER_NAME,
+                                          framefill=True)
+        self.camera = Camera(camera_id,
+                             prop_set={width: width, height: height})
+        self.display = None
+        if show_display:
+            self.display = Display((width, height))
+        self.video_buffer_fname = video_buffer_fname
+
+    def start(self):
+        while True:
+            frame = self.camera.getImage()
+            self.video_streamer.writeFrame(frame)
+            if self.display:
+                frame.save(self.display)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    @staticmethod
+    def save_film_to_disk(self, buffer_name, output_fname):
+        """Use avconv to process the video buffer."""
+        encoding_params = " -i {0} -c:v mpeg4 -b:v 700k -r 24 {1}".format(
+            self.video_buffer_fname, output_fname)
+        call('avconv' + encoding_params, shell=True)
+
 
 if __name__ == '__main__':
-    HELP_MSG = '''record.py [options]
-
-        -c <cam NR>   If you know which cam you want to use: set it here, else the first camera available is selected
-
-        -x <cam Width>    The width of camera capture. Default is 640 pixels
-        --width
-
-        -y <cam Height>   The height of camera capture. Default is 480 pixels
-        --height
-
-        -o <output>       The name of the output file. Default is the timestmp
-        --output
-
-        -h <help>     Show this message
-        '''
-
-
-    camNR = 0
+    camera_id = 0
     width = 640
     height = 480
-    outname = 'output_{0}.mp4'.format(time.ctime().replace(" ", "_"))
+    output_fname = 'output_{0}.mp4'.format(time.ctime().replace(" ", "_"))
 
     try:
         opts, args = getopt.getopt(sys.argv,"hx:y:o:c:",["width=","height=", "output="])
@@ -84,9 +86,14 @@ if __name__ == '__main__':
         elif opt in ('-y', '--height'):
             height = arg
         elif opt in ('-c'):
-            camNR = arg
+            camera_id = arg
         elif opt in ('-o', '--output'):
-            outname = arg
+            output_fname = arg
 
     # Finally let's start
-    main(camNR, width, height, outname)
+    watcher = Watcher(camera_id, output_fname, show_display=True, width=width,
+                      height=height)
+    watcher.start()  # Main loop. Stops when 'q' is pressed.
+    process = Process(target=watcher.save_film_to_disk,
+                      args=(watcher.video_buffer_fname, output_fname))
+    process.start()
